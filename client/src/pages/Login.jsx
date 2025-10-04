1995e420c96a3e1482f16c8df75291123ea2a3c4
Ayush Dubey <154453637+ayushdubey25@users.noreply.github.com>
2025-10-04 21:18:56 +0530

    Update Login.jsx

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -229,7 +229,7 @@ const Login = () => {
         setError('');
         
         try {
-            const res = await axios.post('http://localhost:5000/api/auth/login', { email, password });
+            const res = await axios.post('https://event-planner-nu0c.onrender.com/api/auth/login', { email, password });
             const { token, role, name } = res.data;
 
             // Save token and user info
